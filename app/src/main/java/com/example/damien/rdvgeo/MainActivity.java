fd359001f1cca155368a6f507f331b7bb72c62eb
damien <dami13795@msn.com>
2017-11-27 11:16:51 +0100

    Personnalisation Menu

--- app/src/main/java/com/example/damien/rdvgeo/MainActivity.java
+++ app/src/main/java/com/example/damien/rdvgeo/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.damien.rdvgeo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         //ajoute les entrées de menu_acceuil à l'ActionBar
         getMenuInflater().inflate(R.menu.menu_acceuil, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_send_demand:
+                return true;
+            case R.id.action_check_demand:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
