d7203eee449c818b6e80ee53ffa4195aee1ef5e4
lindonn <nicolaspietraru@gmail.com>
2018-01-13 14:16:41 +0100

    mise en page notif

--- app/src/main/java/com/example/damien/rdvgeo/Activities/NotificationActivity.java
+++ app/src/main/java/com/example/damien/rdvgeo/Activities/NotificationActivity.java
@@ -4,24 +4,41 @@ import android.app.Activity;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.damien.rdvgeo.Notifications;
+import com.example.damien.rdvgeo.R;
 
 public class NotificationActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //On crée un TextView en Java
-        TextView txt=new TextView(this);
-        txt.setText("Voici à l'Activity qui apparait lorsque l'on clique sur la notification !");
-
-        //On ajoute notre TextView à la vue
-        setContentView(txt);
+        setContentView(R.layout.notifications);
 
         //On supprime la notification de la liste de notification comme dans la méthode cancelNotify de l'Activity principale
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.cancel(Notifications.ID_NOTIFICATION);
+
+        // On récupère nos deux boutons créés en XML grâce à leur id
+        Button boutonCreateNotif = findViewById(R.id.AcceptNotif);
+        Button boutonClearNotif = findViewById(R.id.ClearNotif);
+
+        //On applique un écouteur d'évènement à notre bouton "Créer une notification"
+        boutonCreateNotif.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+        //On applique un écouteur d'évènement à notre bouton "Supprimer la notification"
+        boutonClearNotif.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               //TODO
+
+            }
+        });
+
     }
 }
