b9e5950910d20adbbd45da49e30f0ce94e94d982
lindonn <nicolaspietraru@gmail.com>
2018-01-13 17:23:24 +0100

    notifActivity

--- app/src/main/java/com/example/damien/rdvgeo/Activities/NotificationActivity.java
+++ app/src/main/java/com/example/damien/rdvgeo/Activities/NotificationActivity.java
@@ -3,6 +3,7 @@ package com.example.damien.rdvgeo.Activities;
 import android.app.Activity;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,14 +13,29 @@ import com.example.damien.rdvgeo.Notifications;
 import com.example.damien.rdvgeo.R;
 
 public class NotificationActivity extends Activity {
+
+    TextView nomNotif;
+    TextView prenomNotif;
+    TextView dateNotif;
+    TextView lieuNotif;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications);
 
+        nomNotif = findViewById(R.id.nomNotif);
+        prenomNotif = findViewById(R.id.prenomNotif);
+        dateNotif = findViewById(R.id.dateNotif);
+        lieuNotif = findViewById(R.id.lieuNotif);
+
+        Intent i = getIntent();
         //On supprime la notification de la liste de notification comme dans la méthode cancelNotify de l'Activity principale
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.cancel(Notifications.ID_NOTIFICATION);
+
+        //On affiche les données de la notif
+        nomNotif.setText("");
 
         // On récupère nos deux boutons créés en XML grâce à leur id
         Button boutonAcceptNotif = findViewById(R.id.AcceptNotif);
